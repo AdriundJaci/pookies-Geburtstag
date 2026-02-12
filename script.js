a1ed749a07420947f33870a170996b51dd937d83
Adrian <140494832+AdriundJaci@users.noreply.github.com>
2026-02-12 21:41:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "Ich bin jeden Tag so dankbar und glücklich, Dich an meiner Seite zu haben 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
@@ -19,7 +19,7 @@ function typeGreeting() {
 }
 
 // Create floating elements
-const floatingElements = ['💖', '✨', '🌸', '💫', '💕'];
+const floatingElements = ['💖', '✨', '🦦', '😽', '💕'];
 function createFloating() {
     const element = document.createElement('div');
     element.className = 'floating';
